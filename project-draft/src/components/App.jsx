f58c884499b461efbe08128355cf2bb97dec5476
Amelia <amelialx99@gmail.com>
2025-02-16 01:53:47 -0800

    feat: button, homepage draft and data.json

--- project-draft/src/components/App.jsx
+++ project-draft/src/components/App.jsx
@@ -1,13 +1,47 @@
 // Amelia's index.html, main.jsx first render this page
+import React from 'react';
 
-// Import other pages
-import { upload } from "./Upload.jsx"; // meiyao: upload individual warmup
-import { NewPlaylist } from "./NewPlaylist.jsx"; // ellie: upload playlist
+// // Import other pages
+// import { upload } from "./Upload.jsx"; // meiyao: upload individual warmup
+// import { NewPlaylist } from "./NewPlaylist.jsx"; // ellie: upload playlist
 
-// Import Components (1, 2, 3, 4, 8, 9)
-import { NavBar } from "./navigation/NavBar.jsx";
+// // Import Components (1, 2, 3, 4, 8, 9)
+// import { NavBar } from "./navigation/NavBar.jsx";
 
+import { Button } from "./utils/Buttons.jsx";
+import { FilterBar } from "./utils/filterBar.jsx"
 
+import data from '../data/data.json'
 
-
-
+function App(props) {
+    return (
+        <div>
+            <div>
+                <p>button test:</p>
+                <Button text="to Intune" link="https://info340-wi25a.github.io/project-Mayogoose/main/index.html"/>
+            </div>
+            <br></br>
+            <div>
+                <p>filter bar test:</p>
+                <FilterBar />
+            </div>
+            <br></br>
+            <div>
+                <p>homepage content:</p>
+            </div>
+            <div>
+                <div className="grid-container">
+                    {data.map((item) => (
+                    <div key={item.Id}>
+                        <img className="homepageCover" src={item.Img} alt={item.Name}/>
+                        {/* <h1>{item.Name}</h1> */}
+                        <p className="albumName">Soprano Major Scales</p>
+                    </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+  
+export default App;
--- project-draft/src/main.jsx
+++ project-draft/src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+
 import App from './components/App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
