4dd8bc13a2d9404a365e4892e44ad8c5f92366ba
Meiyao <mli1012@uw.edu>
2025-03-05 17:16:24 -0800

    CSS Structure & Style: import CSS once in main.jsx, rather than <Link> in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Load custom css after bootstrap so that it can override bootstrap styles 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import { BrowserRouter } from 'react-router'
